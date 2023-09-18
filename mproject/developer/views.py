286057febcdb78dc5e141cf1adad67c016f19685
Mamoun benmassaoud <mamounbenmassaoud@air-de-mamoun.home>
2023-09-18 23:44:52 +0200

    Introduction faites

--- mproject/developer/views.py
+++ mproject/developer/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+
+from developer.models import Developer
+from django.shortcuts import render, get_object_or_404
+
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the developers index.")
+    context = {
+        'developers': Developer.objects.all()
+    }
+    return render(request, 'developer/index.html', context)
+
+
+def detail(request, developer_id):
+    developer = get_object_or_404(Developer, pk=developer_id)
+    return render(request, 'developer/detail.html', {'developer': developer})
