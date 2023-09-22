7bd2ad492fb309791d48da59f1eb69b20ea18631
ElMamouneBenmassaoud <mamounbenmassaoud@gmail.com>
2023-09-22 12:29:33 +0200

    continuer Vues et gabarits (Vues génériques)

---
+++ mproject/developer/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Developer
+
+class DeveloperForm(forms.ModelForm):
+    class Meta:
+        model = Developer
+        fields = ['first_name', 'last_name']
--- mproject/developer/views.py
+++ mproject/developer/views.py
@@ -4,11 +4,15 @@ from django.shortcuts import render
 
 from developer.models import Developer
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from .forms import DeveloperForm
+from django.urls import reverse
 
 
 def index(request):
     context = {
-        'developers': Developer.objects.all()
+        'developers': Developer.objects.all(),
+        'form': DeveloperForm
     }
     return render(request, 'developer/index.html', context)
 
@@ -16,3 +20,15 @@ def index(request):
 def detail(request, developer_id):
     developer = get_object_or_404(Developer, pk=developer_id)
     return render(request, 'developer/detail.html', {'developer': developer})
+def create(request):
+    form = DeveloperForm(request.POST)
+
+    if form.is_valid():
+        Developer.objects.create(
+        first_name = form.cleaned_data['first_name'],
+        last_name = form.cleaned_data['last_name']
+    )
+    # Toujours renvoyer une HTTPResponseRedirect après avoir géré correctement
+    # les données de la requête POST. Cela empêche les données d'être postée deux
+    # fois si l'utilisateur clique sur le bouton précédent.
+    return HttpResponseRedirect(reverse('developer:index'))
