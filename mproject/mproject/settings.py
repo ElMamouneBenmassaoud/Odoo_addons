fed184a8b604306979d1749b67fb7bca7daa8c36
ElMamouneBenmassaoud <mamounbenmassaoud@gmail.com>
2023-09-25 17:49:14 +0200

    3.3 Amélioration des pages développers à faire

--- mproject/mproject/settings.py
+++ mproject/mproject/settings.py
@@ -82,10 +82,18 @@ WSGI_APPLICATION = 'mproject.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #},
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'mproject',
+         'USER': 'mamounbenmassaoud',
+         'PASSWORD': 'Mamoun123',
+         'HOST': 'localhost',
+         'PORT': '',
+     }
 }
 
 
