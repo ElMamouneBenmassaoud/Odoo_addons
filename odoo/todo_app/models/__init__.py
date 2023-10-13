7b062a8476bd444f750f15f24e8d1e476d4d0650
ElMamouneBenmassaoud <mamounbenmassaoud@gmail.com>
2023-10-13 12:19:11 +0200

    add repository du prof

--- odoo/todo_app/models/__init__.py
+++ odoo/todo_app/models/__init__.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from . import models
+# models/__init__.py
+
+from . import todo_task_model
---
+++ odoo/todo_app/models/todo_task_model.py
@@ -0,0 +1,20 @@
+# todo_task_model.py
+# noinspection PyUnresolvedReferences
+from odoo import models, fields
+
+
+class TodoTask(models.Model):
+    _name = 'todo.task'
+    type = fields.Char(required=True)
+    is_done = fields.Boolean()
+    active = fields.Boolean(default=True)
+    date_deadline = fields.Date()
+    user_id = fields.Many2one(
+        comodel_name='res.users',
+        string='Responsible',
+        default=lambda self: self.env.user,
+    )
+    team_ids = fields.Many2one(
+        comodel_name='res.partner',
+        string='Team',
+    )
