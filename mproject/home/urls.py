40cfe5bba3e3c9d6de8d49ac3b620cba1ee63a15
ElMamouneBenmassaoud <mamounbenmassaoud@gmail.com>
2023-10-27 15:34:14 +0200

    commit to fix bug

--- mproject/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import HomeView, RegisterView
-
-app_name = 'home'
-urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('register/', RegisterView.as_view(), name='register'),
-]
--- mproject/task/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from task.models import Task
-
-
-class TaskForm(forms.ModelForm):
-    class Meta:
-        model = Task
-        fields = ['title', 'description', 'assignee']
