3261f0744f75f3ba7fe477852457edc399bf0136
ElMamouneBenmassaoud <mamounbenmassaoud@gmail.com>
2023-11-03 00:34:05 +0100

    Création de La liste des tâches

--- odoo/todo_stage/__manifest__.py
+++ odoo/todo_stage/__manifest__.py
@@ -25,9 +25,8 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
         'views/todo_menu.xml',
+        'views/todo_view.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
