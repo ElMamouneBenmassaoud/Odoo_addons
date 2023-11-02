287aff386d031e63b46fb17751273b9f3c1dbf0f
ElMamouneBenmassaoud <mamounbenmassaoud@gmail.com>
2023-11-02 20:06:46 +0100

    Partie sécurité réaliser

--- odoo/todo_app/__manifest__.py
+++ odoo/todo_app/__manifest__.py
@@ -28,6 +28,7 @@
     # always loaded
     'data': [
         'security/ir.model.access.csv',
+        'security/security.xml',
         'views/todo_menu.xml',
         'views/todo_view.xml',
         'views/partner.xml',
